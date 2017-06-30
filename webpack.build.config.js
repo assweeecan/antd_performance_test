d24c09ad2e6ddbd10fe00bdaa0e9430b4e5bcd21
assweecan <assweecan>
2017-06-30 18:21:24 +0800

    no message

--- webpack.build.config.js
+++ webpack.build.config.js
@@ -146,6 +146,26 @@ module.exports = function (env = defaultEnv) {
         inject: 'body',
       }),
 
+      // 代码压缩
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        beautify: false,
+        comments: false,
+        sourceMap: false,
+        compress: {
+          // 在UglifyJs删除没有用到的代码时不输出警告
+          warnings: false,
+          // 删除所有的 `console` 语句
+          // 还可以兼容ie浏览器
+          drop_console: true,
+          // 内嵌定义了但是只用到一次的变量
+          collapse_vars: true,
+          // 提取出出现多次但是没有定义成变量去引用的静态值
+          reduce_vars: true,
+        },
+      }),
+
+      // 生产环境
       new webpack.DefinePlugin({
         'process.env.NODE_ENV': JSON.stringify('product'),
       }),
